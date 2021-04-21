5f3c32df46a0d292343585f8214f73b2da94fb6e
Teablack <ddaria.poda@gmail.com>
2021-04-21 18:46:26 +0200

    git wants me to commit, I want to sleep.

--- app/src/main/java/com/example/beekeeper/adapter/ApiaryAdapter.kt
+++ app/src/main/java/com/example/beekeeper/adapter/ApiaryAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.beekeeper.R
 import com.example.beekeeper.model.Apiary
