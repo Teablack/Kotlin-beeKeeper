766f653c79b149aabac2283c8a2805db5cc230d0
Teablack <ddaria.poda@gmail.com>
2021-04-29 15:34:54 +0200

    add creating hive

--- app/src/main/java/com/example/beekeeper/HiveList.kt
+++ app/src/main/java/com/example/beekeeper/HiveList.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ListView
 import androidx.appcompat.widget.Toolbar
