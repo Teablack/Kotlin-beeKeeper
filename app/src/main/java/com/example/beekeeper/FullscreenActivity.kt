1d71df72a1019653e600a7f604b7d4dac4f2601c
Teablack <ddaria.poda@gmail.com>
2021-06-11 19:06:49 +0200

    add animation

--- app/src/main/java/com/example/beekeeper/FullscreenActivity.kt
+++ app/src/main/java/com/example/beekeeper/FullscreenActivity.kt
@@ -1,12 +1,14 @@
 package com.example.beekeeper
 
-import android.content.Context
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
 import com.example.beekeeper.db.DBHelper
 import com.example.beekeeper.login.LoginActivity
 
@@ -17,12 +19,21 @@ class FullscreenActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_fullscreen)
 
+        val logo = findViewById<ImageView>(R.id.logo)
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1.1f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1.1f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(logo, scaleX, scaleY)
+        animator.repeatCount = 5
+        animator.duration = 1000
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.start()
         Thread(){
+
             run {
-                Thread.sleep(2000);
+                Thread.sleep(3000);
                 var sharedPref = this.getSharedPreferences("com.example.beekeeper.shared",0)
                 val islogged = sharedPref.getBoolean("islogged",false)
-                //Log.d("!!!!!TAGG", islogged.toString())
+
                 if(islogged){
                     runOnUiThread(){
                         val username_str = sharedPref.getString("username","")
@@ -33,13 +44,12 @@ class FullscreenActivity : AppCompatActivity() {
                         intent.putExtra("username", username_str)
                         intent.putExtra("userID", userID)
                         startActivity(intent)
-                        //Log.d("!!!!!TAGG", "po start intent")
                         finish()
                     }
                 }
                 else {
-
                     runOnUiThread(){
+
                         val intent = Intent(this, LoginActivity::class.java)
                         startActivity(intent)
                         finish()
